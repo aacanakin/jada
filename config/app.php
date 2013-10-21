d737b1f144aef973e39a044be7d84ad22d9599a8
Aras Can Akin <aacanakin@gmail.com>
2013-10-21 06:52:42 +0300

    Add sample config variable

--- config/app.php
+++ config/app.php
@@ -1,4 +1,14 @@
 <?php
 return array(
 
+	'common' => array(
+		/**
+		 * Common config that is shared w/ each app
+		 */
+		'timezone2' => 'UTC'
+	),
+
+	'SampleCommand' => array(
+		'timezone' => 'GMT'
+	),
 );
