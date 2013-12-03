5f3d68ebb3883d028ec21310d4daa8d97ae8b756
Aras Can Akın <aacanakin@gmail.com>
2013-12-03 11:02:53 +0200

    p function is smarter now, rstrtrim function added

--- src/Jada/Functions.php
+++ src/Jada/Functions.php
@@ -1,4 +1,5 @@
 <?php
+define ('ROOT', dirname(dirname(dirname(__FILE__))));
 /**
  * The global helper functions which can be used in both application & framework level
  * @author Aras Can Akin <aacanakin@gmail.com>
@@ -22,6 +23,10 @@ function d($data, $return = false)
  */
 function p($data, $return = false)
 {
+	if (! is_array($data)) {
+		$data .= "\n";
+	}
+	
 	if ($return) {
 
 		$return = print_r($data, $return);
@@ -42,4 +47,25 @@ function de($data, $return = false)
 {
 	d($data, $return);
 	exit;
+}
+
+function rstrtrim($str, $remove=null) 
+{ 
+    $str    = (string)$str; 
+    $remove = (string)$remove;    
+    
+    if(empty($remove)) 
+    { 
+        return rtrim($str); 
+    } 
+    
+    $len = strlen($remove); 
+    $offset = strlen($str)-$len; 
+    while($offset > 0 && $offset == strpos($str, $remove, $offset)) 
+    { 
+        $str = substr($str, 0, $offset); 
+        $offset = strlen($str)-$len; 
+    } 
+    
+    return rtrim($str);   
 }
